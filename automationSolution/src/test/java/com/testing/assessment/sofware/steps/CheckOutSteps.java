74939592075c2d7e77817e4cb80deab6a9bf23f8
MrNkosiNgcobo <nkosikhonangcobo@yahoo.com>
2023-02-28 20:41:45 +0200

    Update CheckOutSteps

--- automationSolution/src/test/java/com/testing/assessment/sofware/steps/CheckOutSteps.java
+++ automationSolution/src/test/java/com/testing/assessment/sofware/steps/CheckOutSteps.java
@@ -4,13 +4,9 @@ import com.testing.assessment.sofware.datasets.RunConfiguration;
 import io.cucumber.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -20,15 +16,8 @@ public class CheckOutSteps {
     @Autowired
     RunConfiguration runConfiguration;
 
+    @Autowired
     private WebDriver driver;
-
-    @Before
-    public void setup() {
-        // Set the path of the ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "C:\\Workspace\\Env\\chromedriver_win32\\chromedriver.exe");
-        // Create a new instance of the ChromeDriver
-        driver = new ChromeDriver();
-    }
 
     @Given("^The items that the user selected before check out$")
     public void testThisCheckWorks(){
@@ -39,37 +28,61 @@ public class CheckOutSteps {
         // Navigate to the website
         driver.get("https://www.saucedemo.com/");
 
+        // Enter the username and password in the appropriate fields
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // Click the login button
+        driver.findElement(By.id("login-button")).click();
+
         // Find the first product and add it to the cart
-        WebElement product1 = driver.findElement(By.xpath("//div[@class='product'][1]"));
-        product1.findElement(By.xpath(".//button")).click();
+        WebElement product1 = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        product1.click();
 
         // Find the second product and add it to the cart
-        WebElement product2 = driver.findElement(By.xpath("//div[@class='product'][2]"));
-        product2.findElement(By.xpath(".//button")).click();
+        WebElement product2 = driver.findElement(By.id("add-to-cart-sauce-labs-bike-light"));
+        product2.click();
 
         // Go to the cart page
-        driver.findElement(By.cssSelector(".cart")).click();
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
 
         // Confirm that the correct products are added to the cart
-        assertTrue(driver.findElement(By.xpath("//td[text()='Product 1']")).isDisplayed());
-        assertTrue(driver.findElement(By.xpath("//td[text()='Product 2']")).isDisplayed());
+        String backpackDescription = driver.findElement
+                (By.xpath("//span[text()='Your Cart']//following::div[@class='inventory_item_name'][1]"))
+                .getText();
+        String bikeDescription = driver.findElement
+                        (By.xpath("//span[text()='Your Cart']//following::div[@class='inventory_item_name'][2]"))
+                .getText();
+        assertEquals(backpackDescription, "Sauce Labs Backpack");
+        assertEquals(bikeDescription, "Sauce Labs Bike Light");
+
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
 
         // Fill in the customer information and go to the checkout overview screen
         driver.findElement(By.id("first-name")).sendKeys("John");
         driver.findElement(By.id("last-name")).sendKeys("Doe");
         driver.findElement(By.id("postal-code")).sendKeys("12345");
-        driver.findElement(By.cssSelector(".checkout_buttons .btn_primary")).click();
+        driver.findElement(By.id("continue")).click();
 
         // Confirm that the total price is correct
-        String expectedTotalPrice = "$50.00"; // Assuming each product costs $25.00
-        String actualTotalPrice = driver.findElement(By.cssSelector(".summary_total_label")).getText();
-        assertEquals(expectedTotalPrice, actualTotalPrice);
+        String price1= driver.findElement
+                (By.xpath("//span[text()='Checkout: Overview']//following::div[@class='inventory_item_price'][1]")).getText();
+        String price2 = driver.findElement
+                (By.xpath("//span[text()='Checkout: Overview']//following::div[@class='inventory_item_price'][2]")).getText();
+        double backpackPrice = Double.parseDouble(price1.replace("$", ""));
+        double bikePrice = Double.parseDouble(price2.replace("$", ""));
+        double expectedTotalPrice = backpackPrice + bikePrice;
+
+        String stringTotalPrice = driver.findElement
+                (By.xpath("//div[@class='summary_subtotal_label']")).getText();
+        double actualTotalPrice = Double.parseDouble(stringTotalPrice.replaceAll("[^\\d.]+", ""));
+        assertEquals(expectedTotalPrice, actualTotalPrice, 0);
 
         // Finish the order
-        driver.findElement(By.cssSelector(".checkout_buttons .btn_primary")).click();
+        driver.findElement(By.xpath("//button[@id='finish']")).click();
 
         // Confirm that the order confirmation screen is displayed
-        assertTrue(driver.findElement(By.xpath("//h2[text()='THANK YOU FOR YOUR ORDER']")).isDisplayed());
+        assertTrue(driver.findElement(By.xpath("//h2[text()='Thank you for your order!']")).isDisplayed());
 
     }
 }
