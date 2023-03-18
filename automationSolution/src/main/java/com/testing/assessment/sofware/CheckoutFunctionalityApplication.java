f58d91d9cb64b6cdbd95f8469421b353ae04f215
MrNkosiNgcobo <nkosikhonangcobo@yahoo.com>
2023-03-18 15:30:55 +0200

    Small indentation changes

--- automationSolution/src/main/java/com/testing/assessment/sofware/CheckoutFunctionalityApplication.java
+++ automationSolution/src/main/java/com/testing/assessment/sofware/CheckoutFunctionalityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CheckoutFunctionalityApplication {
-
     public static void main(String[] args){
         SpringApplication.run(CheckoutFunctionalityApplication.class);
     }
